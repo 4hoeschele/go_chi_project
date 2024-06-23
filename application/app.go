243b19dec05bb0194b8eb84002608aabaef8c13a
4hoeschele <Carsten Hoeschele>
2024-06-23 15:15:25 +0200

    [main IMPROVE] Added order data type for db columns

--- application/app.go
+++ application/app.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb   *redis.Client
 }
 
 // add the owner of this method(receiver) to the function signature
@@ -19,16 +23,44 @@ func(a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
+	// call ping for the redis client
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("server failed to start: %w", err)
-}
+		return fmt.Errorf("redis failed to start: %w", err)
+	}
+
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis connection: ", err)
+		}
+	}()
+
+	fmt.Println("Starting server...")
+
+	ch := make(chan error, 1)
+
+	go func() {
+	err = server.ListenAndServe()
+	if err != nil {
+		ch <- fmt.Errorf("server failed to start: %w", err)
+	}
+		close(ch)
+}()
+	select {
+	case err = <-ch: 
+	return err 
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
+	}
 	return nil
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb: redis.NewClient(&redis.Options{}),
 	}
 	return app
 }
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
     "fmt"
+	"os"
+	"os/signal"
 
 	"4hoeschele/go_chi_project.git/application"
 )
@@ -10,9 +12,16 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	// should only be used by the main function, initializations and tests
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	// with defer this function will be called when the main function ends
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app: ", err)
 	}
+
 }
 
